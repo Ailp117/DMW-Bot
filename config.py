4794465a893ac785bc80c3db38dfffb59f9f7843
Ailp117 <51854559+Ailp117@users.noreply.github.com>
2026-02-11 23:48:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ import os
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "")
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 
-# Für instant Slash-Sync in deinem privaten Server:
-# GitHub Secret: GUILD_ID = "123456789012345678"
+# Optional: for instant slash command updates in your private server
 GUILD_ID = int(os.getenv("GUILD_ID", "0") or "0")
 
-# Optional: Debug DB SQL Logs
 DB_ECHO = os.getenv("DB_ECHO", "0") == "1"
